f8e1a46cc693fc7f141eb9ded61dbb1219810f0a
Kidand <kidand1412@sina.com>
2020-07-29 10:08:58 +0800

    update to 10-10

--- dianping/src/main/java/com/kidand/dianping/service/impl/ShopServiceImpl.java
+++ dianping/src/main/java/com/kidand/dianping/service/impl/ShopServiceImpl.java
@@ -156,66 +156,189 @@ public class ShopServiceImpl implements ShopService {
 
         Request request = new Request("GET", "/shop/_search");
 
-        String reqJson = "{\n" +
-                "  \"_source\": \"*\",\n" +
-                "  \"script_fields\": {\n" +
-                "    \"distance\": {\n" +
-                "      \"script\": {\n" +
-                "        \"source\": \"haversin(lat,lon,doc['location'].lat,doc['location'].lon)\",\n" +
-                "        \"lang\": \"expression\",\n" +
-                "        \"params\": {\"lat\":" + latitude.toString() + ",\"lon\":" + longitude.toString() + "}\n" +
-                "      }\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"query\": {\n" +
-                "    \"function_score\": {\n" +
-                "      \"query\": {\n" +
-                "        \"bool\": {\n" +
-                "          \"must\": [\n" +
-                "            {\"match\": {\"name\": {\"query\": \"" + keyword + "\"}}},\n" +
-                "            {\"term\": {\"seller_disabled_flag\": 0}}\n" +
-                "          ]\n" +
-                "        }\n" +
-                "      },\n" +
-                "      \"functions\": [\n" +
-                "        {\n" +
-                "          \"gauss\": {\n" +
-                "            \"location\": {\n" +
-                "              \"origin\": \"" + latitude.toString() + "," + longitude.toString() + "\",\n" +
-                "              \"scale\": \"100km\",\n" +
-                "              \"offset\": \"0km\",\n" +
-                "              \"decay\": 0.5\n" +
-                "            }\n" +
-                "          },\n" +
-                "          \"weight\": 9\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"field_value_factor\": {\n" +
-                "            \"field\": \"remark_score\"\n" +
-                "          },\n" +
-                "          \"weight\": 0.2\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"field_value_factor\": {\n" +
-                "            \"field\": \"seller_remark_score\"\n" +
-                "          },\n" +
-                "          \"weight\": 0.1\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"score_mode\": \"sum\",\n" +
-                "      \"boost_mode\": \"sum\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"sort\": [\n" +
-                "    {\n" +
-                "      \"_score\": {\n" +
-                "        \"order\": \"desc\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
+//        String reqJson = "{\n" +
+//                "  \"_source\": \"*\",\n" +
+//                "  \"script_fields\": {\n" +
+//                "    \"distance\": {\n" +
+//                "      \"script\": {\n" +
+//                "        \"source\": \"haversin(lat,lon,doc['location'].lat,doc['location'].lon)\",\n" +
+//                "        \"lang\": \"expression\",\n" +
+//                "        \"params\": {\"lat\":" + latitude.toString() + ",\"lon\":" + longitude.toString() + "}\n" +
+//                "      }\n" +
+//                "    }\n" +
+//                "  },\n" +
+//                "  \"query\": {\n" +
+//                "    \"function_score\": {\n" +
+//                "      \"query\": {\n" +
+//                "        \"bool\": {\n" +
+//                "          \"must\": [\n" +
+//                "            {\"match\": {\"name\": {\"query\": \"" + keyword + "\"}}},\n" +
+//                "            {\"term\": {\"seller_disabled_flag\": 0}}\n" +
+//                "          ]\n" +
+//                "        }\n" +
+//                "      },\n" +
+//                "      \"functions\": [\n" +
+//                "        {\n" +
+//                "          \"gauss\": {\n" +
+//                "            \"location\": {\n" +
+//                "              \"origin\": \"" + latitude.toString() + "," + longitude.toString() + "\",\n" +
+//                "              \"scale\": \"100km\",\n" +
+//                "              \"offset\": \"0km\",\n" +
+//                "              \"decay\": 0.5\n" +
+//                "            }\n" +
+//                "          },\n" +
+//                "          \"weight\": 9\n" +
+//                "        },\n" +
+//                "        {\n" +
+//                "          \"field_value_factor\": {\n" +
+//                "            \"field\": \"remark_score\"\n" +
+//                "          },\n" +
+//                "          \"weight\": 0.2\n" +
+//                "        },\n" +
+//                "        {\n" +
+//                "          \"field_value_factor\": {\n" +
+//                "            \"field\": \"seller_remark_score\"\n" +
+//                "          },\n" +
+//                "          \"weight\": 0.1\n" +
+//                "        }\n" +
+//                "      ],\n" +
+//                "      \"score_mode\": \"sum\",\n" +
+//                "      \"boost_mode\": \"sum\"\n" +
+//                "    }\n" +
+//                "  },\n" +
+//                "  \"sort\": [\n" +
+//                "    {\n" +
+//                "      \"_score\": {\n" +
+//                "        \"order\": \"desc\"\n" +
+//                "      }\n" +
+//                "    }\n" +
+//                "  ]\n" +
+//                "}";
 
-        System.out.println(reqJson);
+        //构建请求
+        JSONObject jsonRequestObj = new JSONObject();
+
+        //构建source部分
+        jsonRequestObj.put("_source", "*");
+
+        //构建自定义距离字段
+        jsonRequestObj.put("script_fields", new JSONObject());
+        jsonRequestObj.getJSONObject("script_fields").put("distance", new JSONObject());
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").put("script", new JSONObject());
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").getJSONObject("script")
+                .put("source", "haversin(lat, lon, doc['location'].lat, doc['location'].lon)");
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").getJSONObject("script")
+                .put("lang", "expression");
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").getJSONObject("script")
+                .put("params", new JSONObject());
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").getJSONObject("script")
+                .getJSONObject("params").put("lat", latitude);
+        jsonRequestObj.getJSONObject("script_fields").getJSONObject("distance").getJSONObject("script")
+                .getJSONObject("params").put("lon", longitude);
+
+        //构建query
+//        Map<String,Object> cixingMap = analyzeCategoryKeyword(keyword);
+//        boolean isAffectFilter = false;
+//        boolean isAffectOrder =  true;
+        jsonRequestObj.put("query", new JSONObject());
+
+        //构建function score
+        jsonRequestObj.getJSONObject("query").put("function_score", new JSONObject());
+
+        //构建function score的query
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("query", new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").put("bool", new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool").put("must", new JSONArray());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").add(new JSONObject());
+
+        //构建match query
+        int queryIndex = 0;
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).put("match", new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).getJSONObject("match").put("name", new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).getJSONObject("match").getJSONObject("name").put("query", keyword);
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).getJSONObject("match").getJSONObject("name").put("boost", 0.1);
+
+        queryIndex++;
+        //构建第二个query的条件
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").add(new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).put("term", new JSONObject());
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                .getJSONArray("must").getJSONObject(queryIndex).getJSONObject("term").put("seller_disabled_flag", 0);
+
+        if (categoryId != null) {
+            queryIndex++;
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                    .getJSONArray("must").add(new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                    .getJSONArray("must").getJSONObject(queryIndex).put("term", new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONObject("query").getJSONObject("bool")
+                    .getJSONArray("must").getJSONObject(queryIndex).getJSONObject("term").put("category_id", categoryId);
+        }
+
+        //构建functions部分
+        jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("functions",new JSONArray());
+
+        int functionIndex = 0;
+        if(orderby == null) {
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").add(new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("gauss", new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("gauss").put("location", new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("gauss")
+                    .getJSONObject("location").put("origin", latitude.toString() + "," + longitude.toString());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("gauss")
+                    .getJSONObject("location").put("scale", "100km");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("gauss")
+                    .getJSONObject("location").put("offset", "0km");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("gauss")
+                    .getJSONObject("location").put("decay", "0.5");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("weight", 9);
+
+            functionIndex++;
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").add(new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("field_value_factor", new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("field_value_factor")
+                    .put("field", "remark_score");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("weight", 0.2);
+
+            functionIndex++;
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").add(new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("field_value_factor", new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("field_value_factor")
+                    .put("field", "seller_remark_score");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("weight", 0.1);
+
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("score_mode","sum");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("boost_mode","sum");
+        }else{
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").add(new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).put("field_value_factor",new JSONObject());
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").getJSONArray("functions").getJSONObject(functionIndex).getJSONObject("field_value_factor")
+                    .put("field","price_per_man");
+
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("score_mode","sum");
+            jsonRequestObj.getJSONObject("query").getJSONObject("function_score").put("boost_mode","replace");
+        }
+
+        //排序字段
+        jsonRequestObj.put("sort",new JSONArray());
+        jsonRequestObj.getJSONArray("sort").add(new JSONObject());
+        jsonRequestObj.getJSONArray("sort").getJSONObject(0).put("_score",new JSONObject());
+        if(orderby == null){
+            jsonRequestObj.getJSONArray("sort").getJSONObject(0).getJSONObject("_score").put("order","desc");
+        }else{
+            jsonRequestObj.getJSONArray("sort").getJSONObject(0).getJSONObject("_score").put("order","asc");
+        }
+
+        String reqJson = jsonRequestObj.toJSONString();
+
+            System.out.println(reqJson);
         request.setJsonEntity(reqJson);
         Response response = highLevelClient.getLowLevelClient().performRequest(request);
 
@@ -232,7 +355,7 @@ public class ShopServiceImpl implements ShopService {
             Integer id = new Integer(jsonObj.get("_id").toString());
             BigDecimal distance = new BigDecimal(jsonObj.getJSONObject("fields").getJSONArray("distance").get(0).toString());
             ShopModel shopModel = get(id);
-            shopModel.setDistance(distance.multiply(new BigDecimal(1000).setScale(0,BigDecimal.ROUND_CEILING)).intValue());
+            shopModel.setDistance(distance.multiply(new BigDecimal(1000).setScale(0, BigDecimal.ROUND_CEILING)).intValue());
             shopModelList.add(shopModel);
         }
 
